a6e480647cfffafdf5d2f14538f089fcdd533c45
Manfred Nilsson <manfred.nilsson@wirelesscar.com>
2020-03-20 08:56:09 +0100

    utility script to clean up all Github deploy keys for org WirelessCar-Cyclone

---
+++ src/bash/utility/clean-up-all-deploy-keys.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+TOKEN=secret
+
+readarray -t REPOS < <(curl -s -H "Accept: application/vnd.github.nebula-preview+json" -H "Accept: application/vnd.github.baptiste-preview+json" -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}"  -X "GET https://api.github.com/orgs/WirelessCar-Cyclone/repos" | jq -r '.[].name')
+
+for REPO in "${REPOS[@]}"
+do
+    printf '%s\n' "${REPO}"
+    readarray -t KEYS < <(curl -s -H "Accept: application/vnd.github.nebula-preview+json" -H "Accept: application/vnd.github.baptiste-preview+json" -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}"  -X GET "https://api.github.com/repos/WirelessCar-Cyclone/${REPO}/keys" | jq -r '.[].id')
+   for KEY in "${KEYS[@]}"
+   do
+        curl -H "Accept: application/vnd.github.nebula-preview+json" -H "Accept: application/vnd.github.baptiste-preview+json" -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}"  -X DELETE "https://api.github.com/repos/WirelessCar-Cyclone/${REPO}/keys/${KEY}"
+   done
+done
