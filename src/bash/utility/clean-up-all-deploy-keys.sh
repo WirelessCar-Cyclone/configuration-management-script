60db52c2cb6fb7d89b80cfc4adb2da4327c32510
Manfred Nilsson <manfred.nilsson@wirelesscar.com>
2020-03-20 11:30:25 +0100

    Double quote to prevent globbing and word splitting

--- src/bash/utility/clean-up-all-deploy-keys.sh
+++ src/bash/utility/clean-up-all-deploy-keys.sh
@@ -7,9 +7,9 @@ readarray -t REPOS < <(curl -s -H "Accept: application/vnd.github.nebula-preview
 for REPO in "${REPOS[@]}"
 do
     printf '%s\n' "${REPO}"
-    readarray -t KEYS < <(curl -s -H "Accept: application/vnd.github.nebula-preview+json" -H "Accept: application/vnd.github.baptiste-preview+json" -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}"  -X GET https://api.github.com/repos/WirelessCar-Cyclone/${REPO}/keys | jq -r '.[].id')
+    readarray -t KEYS < <(curl -s -H "Accept: application/vnd.github.nebula-preview+json" -H "Accept: application/vnd.github.baptiste-preview+json" -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}"  -X GET https://api.github.com/repos/WirelessCar-Cyclone/"${REPO}"/keys | jq -r '.[].id')
    for KEY in "${KEYS[@]}"
    do
-        curl -H "Accept: application/vnd.github.nebula-preview+json" -H "Accept: application/vnd.github.baptiste-preview+json" -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}"  -X DELETE https://api.github.com/repos/WirelessCar-Cyclone/${REPO}/keys/${KEY}
+        curl -H "Accept: application/vnd.github.nebula-preview+json" -H "Accept: application/vnd.github.baptiste-preview+json" -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}"  -X DELETE https://api.github.com/repos/WirelessCar-Cyclone/"${REPO}"/keys/"${KEY}"
    done
 done
